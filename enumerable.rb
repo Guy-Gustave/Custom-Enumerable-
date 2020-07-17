9977a08abe23f7b5768c6301780976e6f48d4117
mohammadumar28 <mohammadumar28@gmail.com>
2020-07-17 12:14:00 +0530

    Implement Select method

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,10 @@
 module Enumerable
   def my_each
-    return enum_for  unless block_given?
-    i =0
+    return enum_for unless block_given?
+    i = 0
     while i < self.length
       yield(self[i])
-      i +=1     
+      i += 1     
     end
     self
   end
@@ -13,30 +13,26 @@ module Enumerable
     return enum_for unless block_given?
     i =0
     while i < self.length
-      yield(self[i],i)
-      i +=1   
+      yield(self[i], i)
+      i += 1   
     end
     self
   end
 
   def my_select
     arr= []
-    x=0
-    while x <self.length
-      arr << self[x] if yield(self[x]) ==true
-      x+=1
+    x = 0
+    while x < self.length
+      arr << self[x] if yield(self[x]) == true
+      x += 1
     end
-    arr
+    print arr
   end
-
-  arr = [3,1,2]
-  arr.my_each_with_index {|el| puts el*2}
 
   arr = [1, 2, 3, 4]
-  arr.my_select {|ele| puts ele*3}
+  arr.my_select { |ele| ele.even? }
 
   def my_all?
-    my_each{|ele| return false if yield(ele)== true}    
+    my_each{ |ele| return false if yield(ele)== true }    
   end
-
 end
