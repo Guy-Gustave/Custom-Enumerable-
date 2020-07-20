5186494395bed802aa9d26000e5330e79b6f319f
mohammadumar28 <mohammadumar28@gmail.com>
2020-07-20 13:38:41 +0530

    Add the approved code

--- enumerable.rb
+++ enumerable.rb
@@ -1,37 +1,48 @@
+# Our enumerable methods
+
 # rubocop:disable Metrics/ModuleLength
 # rubocop:disable Metrics/CyclomaticComplexity
 # rubocop:disable Metrics/MethodLength
 # rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/BlockNesting
+
 module Enumerable
   def my_each
     ary = self
-    ary = ary.is_a?(Array) ? ary : ary.to_a
+    ar = ary.is_a?(Array) ? ary : ary.to_a
     return enum_for unless block_given?
 
     i = 0
-    while i < ary.length
-      yield(ary[i])
+    while i < ar.length
+      yield(ar[i])
       i += 1
     end
-    ary
+
+    return ary unless ary.is_a?(Array)
+
+    ar
   end
 
   def my_each_with_index
     ary = self
-    ary = ary.is_a?(Array) ? ary : ary.to_a
+    ar = ary.is_a?(Array) ? ary : ary.to_a
     return enum_for unless block_given?
 
     i = 0
-    while i < ary.length
-      yield(ary[i], i)
+    while i < ar.length
+      yield(ar[i], i)
       i += 1
     end
-    ary
+    return ary unless ary.is_a?(Array)
+
+    ar
   end
 
   def my_select
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
+    return enum_for unless block_given?
+
     filtered_ary = []
     x = 0
     while x < ary.length
@@ -41,23 +52,53 @@ module Enumerable
     filtered_ary
   end
 
-  def my_all?
+  def my_all?(arg = nil)
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
     value = true
     return true if ary.empty?
 
     i = 0
-    while i < ary.length
-      return false unless yield(ary[i])
+    if arg.nil? && !block_given?
+      while i < ary.length
+        return false unless ary[i]
 
-      i += 1
+        i += 1
+      end
+    elsif arg.nil? && block_given?
+      while i < ary.length
+        return false unless yield(ary[i])
+
+        i += 1
+      end
+    elsif !arg.nil? && !block_given?
+      if arg.is_a?(Class) && !arg.is_a?(Regexp)
+        while i < ary.length
+          return false unless ary[i].is_a?(arg)
+
+          i += 1
+        end
+      elsif arg.is_a?(Regexp)
+        while i < ary.length
+          str = ary[i]
+          str = str.is_a?(String) ? str : str.to_s
+          return false unless arg.match?(str)
+
+          i += 1
+        end
+      elsif !arg.is_a?(Class)
+        while i < ary.length
+          return false unless arg == ary[i]
+
+          i += 1
+        end
+      end
     end
 
     value
   end
 
-  def my_any?
+  def my_any?(arg = nil)
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
 
@@ -65,15 +106,46 @@ module Enumerable
     return true if ary.empty?
 
     i = 0
-    while i < ary.length
-      return true if yield(ary[i]) == true
+    if arg.nil? && !block_given?
+      while i < ary.length
+        return true if ary[i]
 
-      i += 1
+        i += 1
+      end
+    elsif arg.nil? && block_given?
+      while i < ary.length
+        return true if yield(ary[i])
+
+        i += 1
+      end
+    elsif !arg.nil? && !block_given?
+      if arg.is_a?(Class) && !arg.is_a?(Regexp)
+        while i < ary.length
+          return true if ary[i].is_a?(arg)
+
+          i += 1
+        end
+      elsif arg.is_a?(Regexp)
+        while i < ary.length
+          str = ary[i]
+          str = str.is_a?(String) ? str : str.to_s
+          return true if arg.match?(str)
+
+          i += 1
+        end
+      elsif !arg.is_a?(Class)
+        while i < ary.length
+          return true if arg == ary[i]
+
+          i += 1
+        end
+      end
     end
+
     value
   end
 
-  def my_none?
+  def my_none?(arg = nil)
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
 
@@ -81,18 +153,48 @@ module Enumerable
     return true if ary.empty?
 
     i = 0
-    while i < ary.length
-      return false if yield(ary[i]) == true
+    if arg.nil? && !block_given?
+      while i < ary.length
+        return false if ary[i]
 
-      i += 1
+        i += 1
+      end
+    elsif arg.nil? && block_given?
+      while i < ary.length
+        return false if yield(ary[i])
+
+        i += 1
+      end
+    elsif !arg.nil? && !block_given?
+      if arg.is_a?(Class) && !arg.is_a?(Regexp)
+        while i < ary.length
+          return false if ary[i].is_a?(arg)
+
+          i += 1
+        end
+      elsif arg.is_a?(Regexp)
+        while i < ary.length
+          str = ary[i]
+          str = str.is_a?(String) ? str : str.to_s
+          return false if arg.match?(str)
+
+          i += 1
+        end
+      elsif !arg.is_a?(Class)
+        while i < ary.length
+          return false unless arg != ary[i]
+
+          i += 1
+        end
+      end
     end
+
     value
   end
 
   def my_count(arg = nil)
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
-
     return ary.length if arg.nil? && !block_given?
 
     counter = 0
@@ -111,35 +213,20 @@ module Enumerable
     counter
   end
 
-  def my_map
+  def my_map(proc_arg = nil)
     ary = self
     ary = ary.is_a?(Array) ? ary : ary.to_a
     return enum_for unless block_given?
 
+    arr = []
     i = 0
-    arr = []
-    while i < ary.length
-      arr.push(yield(ary[i]))
-      i += 1
-    end
-    arr
-  end
-
-  def my_map_proc_block(proc_arg)
-    ary = self
-    ary = ary.is_a?(Array) ? ary : ary.to_a
-
-    return enum_for unless block_given?
-
-    arr = []
-    if proc_arg.nil?
-      i = 0
+    if !proc_arg.nil?
+      ary.my_each { |arr_item| arr.push(proc_arg.call(arr_item)) }
+    else
       while i < ary.length
         arr.push(yield(ary[i]))
         i += 1
       end
-    else
-      ary.my_each { |arr_item| arr.push(proc_arg(arr_item)) }
     end
     arr
   end
@@ -190,3 +277,4 @@ end
 # rubocop:enable Metrics/CyclomaticComplexity
 # rubocop:enable Metrics/MethodLength
 # rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/BlockNesting
